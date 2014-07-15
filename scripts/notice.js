da3c928535fc280466e507cabe81fd496c4032e1
kate <kate_evstratenko@mail.ru>
2014-07-15 23:33:05 +0300

    added automatic shift of notices

--- scripts/notice.js
+++ scripts/notice.js
@@ -4,20 +4,35 @@
             background: '#20B2AA'
         });*/
         return this.each(function() {
-            var lastNotice = $('body .notice').last();
+            var lastNotice = $('.notice').last();
 
             var newNotice = $('<div/>', {
                 class: 'notice',
-                text: '1111111',
+                text: Math.random(),
             }).css('top', lastNotice.offset() ? Number(lastNotice.offset().top + lastNotice.outerHeight()) : 0);
 
             newNotice.appendTo(this)
                 .animate({
                     opacity: "0.9"
-                }, 700).delay(2000).animate({
+                }, 500).delay(3000).animate({
                     opacity: "0"
-                }, 700);
-            setTimeout(function() { newNotice.remove(); }, 2000);
+                }, 500);
+
+           
+            setTimeout(function () {
+                var offsetTop = newNotice.offset().top
+                var heightNotice = offsetTop + newNotice.outerHeight();
+                //console.log(heightNotice);
+                newNotice.remove();
+
+                $('.notice').offset(function (index, coord) {
+                    var newCoord = {};
+                    newCoord.top = coord.top - heightNotice;
+                    newCoord.left = coord.left;
+                    return newCoord;
+                });
+
+            }, 4000);
 
         });
     };
